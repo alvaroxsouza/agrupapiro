a97d9da72aa5fe7ffc9a277a1e5fe6d55ba154f8
Caio Nery <caionery@escavador.com>
2024-09-01 22:09:45 -0300

    test: Corrige teste trocado

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,69 +1,50 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_import, directives_ordering
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import './step/a_confirmation_email_should_be_sent_to.dart';
-import './step/i_am_on_the_page.dart';
 import './step/i_enter_into_input_field.dart';
-import './step/i_see_a_success_message.dart';
-import './step/i_see_an_error_message.dart';
+import './step/i_see_text.dart';
+import './step/i_should_be_redirected_to_the_applications_homepage.dart';
 import './step/i_tap_button.dart';
 import './step/the_app_is_running.dart';
+import './step/the_user_is_registered_with_password.dart';
 
 void main() {
-  group('''Account Creation''', () {
+  group('User Login', () {
     Future<void> bddSetUp(WidgetTester tester) async {
       await theAppIsRunning(tester);
-      await iAmOnThePage(tester, 'Criar conta');
+      await theUserIsRegisteredWithPassword(
+          tester, 'mail@mail.com', 'password'); // Usuário pré-registrado
     }
 
-    testWidgets('Successful Account Creation', (tester) async {
+    testWidgets('Successful Login', (tester) async {
       await bddSetUp(tester);
-
-      // Preencher todos os campos do formulário
-      await iEnterIntoInputField(tester, 'fulano Di tal', 0); // Nome
-      await iEnterIntoInputField(tester, '12345678909', 1); // CPF
-      await iEnterIntoInputField(tester, 'mail@mail.com', 2); // Email
-      await iEnterIntoInputField(tester, 'password', 3); // Senha
-      await iEnterIntoInputField(tester, 'password', 4); // Confirmar Senha
-      await iEnterIntoInputField(tester, 'UFBA', 5); // Universidade
-      await iEnterIntoInputField(tester, 'Ciência da Computação', 6); // Curso
-      await iEnterIntoInputField(tester, '1', 7); // Período
-      await iEnterIntoInputField(tester, '71999999999', 8); // Telefone
-
-      // Clicar no botão 'Cadastrar'
-      await iTapButton(tester, 'Cadastrar');
-
-      // Verificar se a mensagem de sucesso aparece
-      await iSeeASuccessMessage(tester, 'Usuário cadastrado com sucesso!');
-
-      //TODO: Implementar banco em memória
-
-      // Verificar se um email de confirmação foi enviado
-      await aConfirmationEmailShouldBeSentTo(tester, 'mail@mail.com');
+      await iEnterIntoInputField(
+          tester, 'mail@mail.com', 0); // Campo de email (índice 0)
+      await iEnterIntoInputField(
+          tester, 'password', 1); // Campo de senha (índice 1)
+      await iTapButton(tester, 'Entrar'); // Botão de login
+      await iShouldBeRedirectedToTheApplicationsHomepage(
+          tester); // Verificação de redirecionamento
     });
 
-    testWidgets('''Invalid Email Format''', (tester) async {
+    testWidgets('Invalid Username', (tester) async {
       await bddSetUp(tester);
+      await iEnterIntoInputField(
+          tester, 'wronguser@mail.com', 0); // Email inválido (índice 0)
+      await iEnterIntoInputField(tester, 'password', 1); // Senha (índice 1)
+      await iTapButton(tester, 'Entrar');
+      await iSeeText(tester,
+          'Falha no login. Verifique suas credenciais.'); // Mensagem de erro
+    });
 
-      // Preencher todos os campos do formulário
-      await iEnterIntoInputField(tester, 'fulano Di tal', 0); // Nome
-      await iEnterIntoInputField(tester, '12345678909', 1); // CPF
-      await iEnterIntoInputField(tester, 'mail_mail.com', 2); // Email
-      await iEnterIntoInputField(tester, 'password', 3); // Senha
-      await iEnterIntoInputField(tester, 'password', 4); // Confirmar Senha
-      await iEnterIntoInputField(tester, 'UFBA', 5); // Universidade
-      await iEnterIntoInputField(tester, 'Ciência da Computação', 6); // Curso
-      await iEnterIntoInputField(tester, '1', 7); // Período
-      await iEnterIntoInputField(tester, '71999999999', 8); // Telefone
-
-      // Clicar no botão '
-      await iTapButton(
-          tester, 'Cadastrar'); // Texto do botão atualizado para 'Cadastrar'
-
-      await iSeeAnErrorMessage(tester, 'Email inválido');
+    testWidgets('Invalid Password', (tester) async {
+      await bddSetUp(tester);
+      await iEnterIntoInputField(
+          tester, 'mail@mail.com', 0); // Email (índice 0)
+      await iEnterIntoInputField(
+          tester, 'wrongpassword', 1); // Senha inválida (índice 1)
+      await iTapButton(tester, 'Entrar');
+      await iSeeText(tester,
+          'Falha no login. Verifique suas credenciais.'); // Mensagem de erro
     });
   });
 }
